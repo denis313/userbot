2622ce6ab1680ab5afebac7c89e485d9d6949ad4
Denis Evtushok <evtushokdenis2004@gmail.com>
2023-12-18 17:58:22 +0300

    new

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from pyrogram import Client,  filters
 from pyrogram.handlers import MessageHandler
 
 # Target chat. Can also be a list of multiple chat ids/usernam
-api_id = 27477677
-api_hash = "622dcf790c9b32e5b94aac8c79f0fbe5"
+api_id = 29864138
+api_hash = "c8b7fae41465d5685c2df4e74e0cb6ef"
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
@@ -16,15 +16,12 @@ app = Client("my_account", api_id=api_id, api_hash=api_hash)
 #
 
 async def hello(app, message):
-    await app.copy_message(-1001795745076, -1001599236833, message.id)
+    await app.copy_message(-1001795745076, -1001599236833, message.id, reply_to_message_id=3566)
+    # await app.copy_message(-1001795745076, -1001599236833, message.id, reply_to_message_id=1090900)
     print(message.chat.id, message.text)
+    print(message.text, message)
 
-
-# async def hello2(app, message):
-#     await app.copy_message(-1002061621024, message.chat.id, message.id)
-#     print(message.chat.id, message.text)
-# app.add_handler(MessageHandler(hello, filters.channel))
-# app.add_handler(MessageHandler(hello2, filters.group))
+app.add_handler(MessageHandler(hello), filters.channel)
 
 
 app.run()
