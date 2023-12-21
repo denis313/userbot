9f902d7647f462e6ee1bc0f3f644fc1a9c72ac15
Denis Evtushok <evtushokdenis2004@gmail.com>
2023-12-21 22:27:40 +0300

    new commit

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ api_hash = "c8b7fae41465d5685c2df4e74e0cb6ef"
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
 
-# async def chat():
-#     async with app:
-#
-#         # Join chat via username
-#         await app.join_chat('https://t.me/aiogram_ru/1417272')
-#
-
 # async def hello(app, message):
 #     await app.copy_message(-1001795745076, -1001599236833, message.id, reply_to_message_id=3566)
 #     # await app.copy_message(-1001795745076, -1001599236833, message.id, reply_to_message_id=1090900)
