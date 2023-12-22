57468882e47f8f76317457e3d41f074f39af70fc
Denis Evtushok <evtushokdenis2004@gmail.com>
2023-12-22 22:10:40 +0300

    new commit

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ api_hash = "c8b7fae41465d5685c2df4e74e0cb6ef"
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
 
-# async def hello(app, message):
-#     await app.copy_message(-1001795745076, -1001599236833, message.id, reply_to_message_id=3566)
-#     # await app.copy_message(-1001795745076, -1001599236833, message.id, reply_to_message_id=1090900)
-#     print(message.chat.id, message.text)
-#     print(message.text, message)
-#
-# app.add_handler(MessageHandler(hello), filters.channel)
+async def hello(app, message):
+    await app.copy_message(-1001795745076, -1001599236833, message.id, reply_to_message_id=3566)
+    # await app.copy_message(-1001795745076, -1001599236833, message.id, reply_to_message_id=1090900)
+    print(message.chat.id, message.text)
+    print(message.text, message)
+
+app.add_handler(MessageHandler(hello), filters.channel)
 
 
 app.run()
